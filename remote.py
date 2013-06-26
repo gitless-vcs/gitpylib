8770ce140edc45c174905d4795e1d66cee111185
Santiago Perez De Rosso <sperezde@csail.mit.edu>
2013-06-26 15:12:25 -0400

    Remote support

---
+++ remote.py
@@ -0,0 +1,43 @@
+"""Module for dealing with Git remotes."""
+
+
+import common
+
+SUCCESS = 1
+REMOTE_NOT_FOUND = 2
+REMOTE_UNREACHABLE = 3
+
+
+def add(remote_name, remote_url):
+  """Adds the given remote."""
+  common.safe_git_call('remote add %s %s' % (remote_name, remote_url))
+
+
+def list():
+  """Returns a list with all the remotes."""
+  out, unused_err = common.safe_git_call('remote')
+  return out.splitlines()
+
+
+def show(remote_name):
+  """Get information about the given remote.
+
+  Args:
+    remote_name: the name of the remote to get info from.
+  
+  Returns:
+    None if remote doesn't exist or the output of the cmd if otherwise.
+  """
+  ok, out, err = common.git_call('remote show %s' % remote_name)
+  if not ok:
+    if 'fatal: Could not read from remote repository' in err:
+      return (REMOTE_UNREACHABLE, None)
+    else:
+      raise Exception('Unexpected output %s, err %s' % (out, err))
+  if not out:
+    return (REMOTE_NOT_FOUND, None)
+  return (SUCCESS, out) 
+
+
+def rm(remote_name):
+  common.safe_git_call('remote rm %s' % remote_name)
