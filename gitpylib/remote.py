45903206faa5fcb054522be027f36b937841fb2d
Santiago Perez De Rosso <sperezde@csail.mit.edu>
2014-01-20 18:46:36 -0500

    Improvements in the remote module (renamed method list_all to show_all)

--- gitpylib/remote.py
+++ gitpylib/remote.py
@@ -7,6 +7,7 @@
 
 import common
 
+
 SUCCESS = 1
 REMOTE_NOT_FOUND = 2
 REMOTE_UNREACHABLE = 3
@@ -14,14 +15,19 @@ REMOTE_BRANCH_NOT_FOUND = 4
 
 
 def add(remote_name, remote_url):
-  """Adds the given remote."""
+  """Adds the given remote.
+
+  Args:
+    remote_name: the name of the remote to add.
+    remote_url: the url of the remote to add.
+
+  Returns:
+    SUCCESS or REMOTE_UNREACHABLE.
+  """
+  if _show(remote_url)[0] == REMOTE_UNREACHABLE:
+    return REMOTE_UNREACHABLE
   common.safe_git_call('remote add %s %s' % (remote_name, remote_url))
-
-
-def list():
-  """Returns a list with all the remotes."""
-  out, unused_err = common.safe_git_call('remote')
-  return out.splitlines()
+  return SUCCESS
 
 
 def show(remote_name):
@@ -31,17 +37,18 @@ def show(remote_name):
     remote_name: the name of the remote to get info from.
 
   Returns:
-    None if remote doesn't exist or the output of the cmd if otherwise.
+    a tuple (status, out) where status is one of SUCCESS, REMOTE_NOT_FOUND, or
+    REMOTE_UNREACHABLE and out is the output of the show command on success.
   """
-  ok, out, err = common.git_call('remote show %s' % remote_name)
-  if not ok:
-    if 'fatal: Could not read from remote repository' in err:
-      return (REMOTE_UNREACHABLE, None)
-    else:
-      raise Exception('Unexpected output %s, err %s' % (out, err))
-  if not out:
+  if remote_name not in show_all():
     return (REMOTE_NOT_FOUND, None)
-  return (SUCCESS, out)
+  return _show(remote_name)
+
+
+def show_all():
+  """Get information of all the remotes."""
+  out, unused_err = common.safe_git_call('remote')
+  return out.splitlines()
 
 
 def rm(remote_name):
@@ -54,3 +61,16 @@ def head_exist(remote_name, head):
   if not ok:
     return (False, REMOTE_UNREACHABLE)
   return (len(out) > 0, REMOTE_BRANCH_NOT_FOUND)
+
+
+# Private functions.
+
+
+def _show(remote):
+  ok, out, err = common.git_call('remote show %s' % remote)
+  if not ok:
+    if 'fatal: Could not read from remote repository' in err:
+      return (REMOTE_UNREACHABLE, None)
+    else:
+      raise Exception('Unexpected output %s, err %s' % (out, err))
+  return (SUCCESS, out)
