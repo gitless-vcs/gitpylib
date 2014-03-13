db98996d117ab6bb88b80691d375f039dec6ea42
Santiago Perez De Rosso <sperezde@csail.mit.edu>
2014-03-13 16:33:16 -0400

    machine-friendly wrap of git log

--- gitpylib/log.py
+++ gitpylib/log.py
@@ -3,10 +3,74 @@
 # Licensed under GNU GPL, version 2.
 
 
-import subprocess
+import collections
+import re
+
+from . import common
+from . import file as git_file
+
+
+# TODO: add more info like parents, tree, committer, etc.
+Commit = collections.namedtuple(
+  'Commit', ['id', 'author', 'msg', 'diffs'])
+
+CommitAuthor = collections.namedtuple(
+  'CommitAuthor', ['name', 'email', 'date', 'date_relative'])
 
 
 def log(include_diffs=False):
-  # The pipe to less shouldn't be here. TODO: fix.
-  subprocess.call(
-      'git log {0} | less'.format('-p' if include_diffs else ''), shell=True)
+  log_fmt = r'[[%H] [%an] [%ae] [%aD] [%ar]]%n%B'
+  out, _ = common.safe_git_call(
+      'log --format=format:"{0}" {1}'.format(
+        log_fmt, '-p' if include_diffs else ''))
+  return _parse_log_output(out)
+
+
+def _parse_log_output(out):
+  def _create_ci(m, msg, diffs):
+    processed_diffs = []
+    for diff in diffs:
+      processed_diffs.append(
+          git_file._process_diff_output(git_file._split_diff(diff)[1]))
+    return Commit(
+        m.group(1), CommitAuthor(*m.group(2, 3, 4, 5)), '\n'.join(msg),
+        processed_diffs)
+
+  if not out:
+    return []
+
+  pattern = re.compile(r'\[\[(.*)\] \[(.*)\] \[(.*)\] \[(.*)\] \[(.*)\]\]')
+  out_it = iter(out.splitlines())
+  m = None
+  msg = None
+  diffs = []
+  curr_diff = None
+  ret = []
+  try:
+    line = out_it.next()
+    while True:
+      m = re.match(pattern, line)
+      if not m:
+        raise common.UnexpectedOutputError('log', line)
+      line = out_it.next()
+      msg = []
+      while not line.startswith('diff --git') and not line.startswith('[['):
+        msg.append(line)
+        line = out_it.next()
+      diffs = []
+      curr_diff = []
+      while not line.startswith('[['):
+        curr_diff.append(line)
+        line = out_it.next()
+        while not line.startswith('diff --git') and not line.startswith('[['):
+          curr_diff.append(line)
+          line = out_it.next()
+        diffs.append(curr_diff)
+        curr_diff = []
+
+      ret.append(_create_ci(m, msg, diffs))
+  except StopIteration:
+    if curr_diff:
+      diffs.append(curr_diff)
+    ret.append(_create_ci(m, msg, diffs))
+  return ret
